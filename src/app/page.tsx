ae6d1a9f50fd505a2a66909bed96cd21513915b9
arjav0703 <arjavjain0703@gmail.com>
2025-02-23 11:58:58 +0530

    hmm

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,52 @@
+"use client";
 
-import React, { PureComponent } from 'react'
+import React, { useState } from "react";
 
-export class page extends PureComponent {
-  render() {
+import {
+  Heading,
+  Text,
+  Button,
+  Icon,
+  InlineCode,
+  Logo,
+  Input,
+  Avatar,
+  AvatarGroup,
+  Textarea,
+  PasswordInput,
+  SegmentedControl,
+  SmartLink,
+  Dialog,
+  Feedback,
+  SmartImage,
+  Line,
+  LogoCloud,
+  Background,
+  Select,
+  useToast,
+  Card,
+  Fade,
+  StatusIndicator,
+  DateRangePicker,
+  DateRange,
+  TiltFx,
+  HoloFx,
+  IconButton,
+  TagInput,
+  Switch,
+  Column,
+  Row,
+  StyleOverlay,
+  Accordion,
+} from "@/once-ui/components";
+import { CodeBlock, MediaUpload } from "@/once-ui/modules";
+
+export default function page() {
+
     return (
       <div>
-        
+      <Heading>Page</Heading>
       </div>
     )
-  }
-}
 
-export default page
+}
