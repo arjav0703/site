f3e29c3b044466f2f407c43b74743aaf4d9cf81b
arjav0703 <arjavjain0703@gmail.com>
2025-02-25 22:09:31 +0530

    fuck chrome and fuck google

--- src/app/components/Myimage.tsx
+++ src/app/components/Myimage.tsx
@@ -10,28 +10,19 @@ function Myimage(){
         position="relative"
         maxWidth={24}
         aspectRatio={0.75}
-        //overflow="hidden"
+        overflow="hidden"
         
       >
         <SmartImage
             src="https://cdn.hack.pet/slackcdn/af5e31e9f4358229cee49d68786916e7.jpg"
             alt="Arjav Jain"
             data-border='playful'
+            sizes="640px"
+            priority={true}
             />
         
         
-        <Fade
-          data-border='playful'
-          fillWidth
-          position="absolute"
-          bottom="0"
-          to="top"
-          height={12}
-          pattern={{
-            display: true,
-            size: '2'
-          }}
-        />
+        
       </Flex>
     )
 }
